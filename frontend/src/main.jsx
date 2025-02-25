414a554cb7ccadd52c82a5c43f2f64f8e9d5eca0
Rebeca <rebecarv27@gmail.com>
2025-02-25 11:41:39 -0700

    AVEER

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import Login from './pages/Login.jsx'
+import { ThemeProvider } from './context/ThemeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Login/>
-  </React.StrictMode>
+    <ThemeProvider>
+    <App />
+    {/* <Login/> */}
+    </ThemeProvider>
+  </React.StrictMode>,
 )
