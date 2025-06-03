771a8b07686f4491aa6e824863f4f9d9165aa4f3
Rebeca <rebecarv27@gmail.com>
2025-06-03 11:03:21 -0700

    hahaha

--- frontend/src/components/modales/Perfil/AvatarModal.jsx
+++ frontend/src/components/modales/Perfil/AvatarModal.jsx
@@ -1,15 +1,20 @@
 
 import React from 'react';
-
+import avatar1 from '../../../assets/profile/Perfil1.png'
+import avatar2 from '../../../assets/profile/Perfil2.png'
+import avatar3 from '../../../assets/profile/Perfil3.png'
+import avatar4 from '../../../assets/profile/Perfil4.png'
+import avatar5 from '../../../assets/profile/Perfil5.png'
+import avatar6 from '../../../assets/profile/Perfil6.png'
 
 // Lista de nombres de archivo de avatares disponibles
 const avatars = [
-  'Perfil1.png',
-  'Perfil2.png',
-  'Perfil3.png',
-  'Perfil4.png',
-  'Perfil5.png',
-  'Perfil6.png',
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  avatar5,
+  avatar6,
 ];
 
 const AvatarModal = ({ isOpen, onClose, onSelect }) => {
@@ -24,7 +29,7 @@ const AvatarModal = ({ isOpen, onClose, onSelect }) => {
           {avatars.map((avatar) => (
             <img
               key={avatar}
-              src={`../../../assets/profile/${avatar}`}
+              src={avatar}
               alt={avatar}
               className="w-20 h-20 rounded-full object-cover cursor-pointer hover:scale-105 transition-transform border-2 hover:border-blue-500"
               onClick={() => onSelect(avatar)}
