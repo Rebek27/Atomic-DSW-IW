b0927d4c34514a19d5fd1c072d32b0c53ea44be5
Rebeca <rebecarv27@gmail.com>
2025-06-03 11:38:45 -0700

    cambitos

--- frontend/src/pages/Faq.jsx
+++ frontend/src/pages/Faq.jsx
@@ -26,7 +26,7 @@ const Faq = () => {
 
 
     return (
-        <div className=" flex justify-center items-center  bg-gray-50 dark:bg-gray-900 p-6">
+        <div className=" flex justify-center items-center  bg-gray-50 dark:bg-gray-900 p-2">
             {/* Cuadro redondeado contenedor principal */}
             <div className="relative w-full  h-[80vh] rounded-2xl shadow-xl overflow-hidden bg-white dark:bg-gray-800">
 
@@ -39,8 +39,8 @@ const Faq = () => {
 
                 {/* Sección principal sobrepuesta */}
 
-                <section className="relative bg-white rounded-xl shadow p-6 mx-auto max-w-4xl mt-14 items-center dark:bg-gray-800 border border-gray-300">
-                    <div className="flex items-center space-x-8">
+                <section className="relative bg-white rounded-xl shadow p-2 mx-auto max-w-4xl mt-8 items-center dark:bg-gray-800 border border-gray-300">
+                    <div className="flex items-center space-x-2">
                         <div className="flex items-center gap-2">
                             <FaQuestionCircle className="text-gray-500 w-6 h-6" />
                             <h2 className="text-xl font-semibold">Preguntas frecuentes</h2>
@@ -49,17 +49,17 @@ const Faq = () => {
 
                 </section>
 
-                <section className="relative bg-white/100 rounded-xl shadow p-8 max-w-4xl  mx-auto mt-10 dark:bg-gray-800 border border-gray-300 ">
+                <section className="relative bg-white/100 rounded-xl shadow p-2 max-w-4xl  mx-auto mt-4 dark:bg-gray-800 border border-gray-300 ">
                     <div className="space-y-8">
                         {faqs.map((faq, index) => (
                             <div key={index} className="border border-gray-200 rounded-xl dark:border-gray-700">
                                 <button
                                     onClick={() => toggle(index)}
-                                    className="flex items-center justify-between w-full p-6 text-base font-medium text-left text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+                                    className="flex items-center justify-between w-full p-6 text-s font-small text-left text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
                                 >
                                     <span>{faq.question}</span>
                                     <svg
-                                        className={`w-4 h-4 transform transition-transform duration-200 ${openIndex === index ? 'rotate-180' : ''}`}
+                                        className={`w-2 h-4 transform transition-transform duration-200 ${openIndex === index ? 'rotate-180' : ''}`}
                                         fill="none"
                                         stroke="currentColor"
                                         strokeWidth="2"
@@ -71,7 +71,7 @@ const Faq = () => {
                                     </svg>
                                 </button>
                                 {openIndex === index && (
-                                    <div className="p-5 text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-900">
+                                    <div className="p-2 text-gray-500 text-small dark:text-gray-400 bg-white dark:bg-gray-900">
                                         {faq.answer}
                                     </div>
                                 )}
